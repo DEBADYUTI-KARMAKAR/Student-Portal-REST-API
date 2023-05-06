ab9c269a6ae3a7105dd58481336a2df7efddf817
Debadyuti <debadyuti.karmakar@gmail.com>
2023-05-07 01:51:00 +0530

    Initial setup

---
+++ src/main/java/com/example/restapi/StudentManagemment/StudentManagemmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapi.StudentManagemment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagemmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagemmentApplication.class, args);
+	}
+
+}
