05c7f10c745b96dbe2ee04ebcbde5ca93c65b1dd
Debadyuti <debadyuti.karmakar@gmail.com>
2023-05-12 22:33:38 +0530

    Content Negociation

---
+++ src/main/java/com/example/restapi/StudentManagemment/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.restapi.StudentManagemment.config;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorParameter(true)
+                .parameterName("mediaType")
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("xml",MediaType.APPLICATION_XML)
+                .mediaType("json",MediaType.APPLICATION_JSON);
+    }
+}
